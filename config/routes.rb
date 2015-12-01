02d0ffc205f1af5a90452e02a50d281d821e91d0
Kyle Cline <kyledcline@gmail.com>
2015-12-01 12:44:08 -0600

    Setup RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(veresion: 1, default: true) do
     end
   end
 end
