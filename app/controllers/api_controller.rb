3f27b08f22ea283c24e92399c29118a45d3cb8ac
Kyle Cline <kyledcline@gmail.com>
2015-12-03 17:17:10 -0600

    Enable user authentication

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,13 @@
 class ApiController < ApplicationController
+  include Authenticable
+
   respond_to :json
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  private
+
+  def render_not_found
+    render json: { errors: 'Not Found' }, status: :not_found
+  end
 end
