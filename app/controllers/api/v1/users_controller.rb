b713b6cabd8f6f64561a6d39560075910c25ade0
Kyle Cline <kyledcline@gmail.com>
2016-01-24 14:31:33 -0600

    Configure Pundit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,34 +1,30 @@
-module Api
-  module V1
-    class UsersController < AuthenticatedController
-      skip_before_action :authenticate_with_token!, only: :create
+class Api::V1::UsersController < AuthenticatedController
+  skip_before_action :authenticate_with_token!, only: [:show, :create]
 
-      def show
-        respond_with @user
-      end
+  def show
+    respond_with current_user
+  end
 
-      def create
-        @user = User.new(user_params)
-        if @user.save
-          render json: @user, status: :created, location: [:api, @user]
-        else
-          render_model_errors @user.errors
-        end
-      end
-
-      def update
-        if @user.update(user_params)
-          render json: @user, status: 201, location: [:api, @user]
-        else
-          render_model_errors @user.errors
-        end
-      end
-
-      private
-
-      def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created, location: [:api, @user]
+    else
+      render_model_errors @user.errors
     end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: 201, location: [:api, @user]
+    else
+      render_model_errors @user.errors
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,4 +2,8 @@ class ApiController < ApplicationController
   include ErrorRendering
   respond_to :json
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  # pundit checks every action to make sure authorization is checked
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 end
